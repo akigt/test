044bcb68b3042b781f625076f4f7fdf08e1470f9
Aki <mifukia1101@gmail.com>
2015-07-14 23:20:16 +0900

    Three strikes and refactor p59{

--- lists/tests.py
+++ lists/tests.py
@@ -20,3 +20,17 @@ class HomePageTest(TestCase):
 		# self.assertIn(b'<title>To-Do lists</title>',response.content)
 		# self.assertTrue(response.content.endswith(b'</html>'))
 		self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		self.assertEqual(response.content.decode(), expected_html)
